5572ad5d38ceaf1dee0e9863f25c2533639962b2
AL IMRAN <imu.png@gmail.com>
2020-04-07 23:04:00 +0600

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+(function ($) {
+    "use strict";
+
+
+
+    $(".mobile_menu_btn, .mobile_menu ul li a").click(function(){
+        $(".header , .mobile_menu").toggleClass("mobile_menu_on");
+    });
+
+
+}(jQuery));
+
